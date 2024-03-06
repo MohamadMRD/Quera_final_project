e786c4ae93177056b77149e2d788145d73678a3e
MohamadMRD <162503072+MohamadMRD@users.noreply.github.com>
2024-03-07 00:04:00 +0330

    login view

--- feze1_finall_projecs/file1.py
+++ feze1_finall_projecs/file1.py
@@ -1,1 +1,24 @@
-print("hello")
+from django.views.generic import View
+
+
+class Login(View):
+    template_name = 'authentication/login.html'
+    form_class = forms.LoginForm
+    
+    def get(self, request):
+        form = self.form_class()
+        message = ''
+        return render(request, self.template_name, context={'form': form, 'message': message})
+        
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            user = authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password'],
+            )
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+        message = 'Login failed!'
+        return render(request, self.template_name, context={'form': form, 'message': message})
